96fbc83c7e390958a90cbd11f72d741a04835d88
covrigel <alexandra.nemes1986@gmail.com>
2016-09-01 18:32:58 +0000

    included modules and package.json file

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-var hello = require("./custom_hello");
-var gb = require("./custom_goodbye");
-var makeRequest = require("./http_request");
+// var hello = require("./custom_hello");
+// var gb = require("./custom_goodbye");
+// var makeRequest = require("./http_request");
+// var express = require("express");
+// var app = express();
 
-makeRequest("A new message");
-hello();
-gb.goodbye();
+// app.get("/", function(request, response){
+//   response.sendFile(__dirname + "index.html")
+// });
+
+// app.listen(3000);
+
+// makeRequest("A new message");
+// hello();
+// gb.goodbye();
+
+var request = require("request");
+var url = require("url");
+
+app.get("twwts/:username", function(request, response){
+  var username = require.params.username;
+  options = {
+    protocol: "http:",
+    host: "api.twitter.com",
+    pathname: "/1/statuses/user_timeline.json",
+    query: {screen_name: username, count: 10}
+  }
+  var twitter_url = url.format(options);
+  request(twitterUrl).pipe(response);
+});
--- my_modules.js
+++ my_modules.js
@@ -1,6 +1,6 @@
-var foo = function(){}
-var bar = function(){}
-var bax = function(){}
+var foo = function(){console.log("this is foo function")}
+var bar = function(){console.log("this is bar function")}
+var baz = function(){console.log("this is baz function")}
 
 exports foo = foo;
 exports bar = bar;
